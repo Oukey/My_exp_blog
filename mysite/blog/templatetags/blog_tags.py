a8f6fdf6dd3a8920f338bcfad0ec93c4fafb5369
Oukey <oukey311@gmail.com>
2019-12-17 22:35:47 +0400

    добавлен тэг вывода последних постов

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -11,3 +11,9 @@ register = template.Library()
 def total_posts():
     """Тег возвращает количество опубликованных статей"""
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
