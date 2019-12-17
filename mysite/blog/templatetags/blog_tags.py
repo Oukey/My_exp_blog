a1539fa82cdb20ccabbcad75c5c4bcce1563280c
Oukey <oukey311@gmail.com>
2019-12-17 22:55:56 +0400

    lобавлен тэг вывода наиболее комментируемых постов

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -2,6 +2,7 @@
 """Модуль для собственных тегов"""
 
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 register = template.Library()
@@ -17,3 +18,8 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
