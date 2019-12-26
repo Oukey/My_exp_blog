2ace9d860a4c63ee383befccdd5cebdc8ac6f78b
Oukey <oukey311@gmail.com>
2019-12-26 22:59:15 +0400

    Добавлена функция поиска

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -105,13 +105,13 @@ def post_detail(request, year, month, day, post):
 def post_search(request):
     form = SearchForm()
     query = None
-    result = []
+    results = []
     if 'query' in request.GET:
         form = SearchForm(request.GET)
-    if form.is_valid():
-        query = form.cleaned_data['query']
-        results = Post.objects.annotate(
-            seqrch=SearchVector('title', 'body'),).filter(search=query)
-        return render(request, 'blog/post/search.html', {'form': form,
-                                                         'query': query,
-                                                         'results': results})
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.objects.annotate(search=SearchVector('title', 'body'),
+                                            ).filter(search=query)
+    return render(request, 'blog/post/search.html', {'form': form,
+                                                     'query': query,
+                                                     'results': results})
